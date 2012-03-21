b8841d8e995a201e918988b89cdd5fa295a8f4e0
William Salazar <was@williamsalazar.com>
2012-03-21 16:14:48 -0400

    Committing with no changes made.

---
+++ app/Model/BukketList.php
@@ -0,0 +1,22 @@
+<?php
+class BukketList extends AppModel{
+	public $name = 'BukketList';	
+	public $validate = array(
+      'title' => array(
+      		'this_field_can_not_be_empty' => array(
+	        	'rule' => 'notEmpty',
+	        	'message' => 'Hey buddy, you forgot to fill this in.'
+          		),
+          	'must_be_unique' => array(
+          		'rule' => 'isUnique',
+          		'message' => 'You already have this in your bukket list.',
+          		)
+      ),
+      'items' => array(
+      		'this_field_can_not_be_empty' => array(
+      			'rule' => 'notEmpty',
+      			'message' => 'Um... you forgot to fill this one too.'
+            )
+        )
+  );
+}
