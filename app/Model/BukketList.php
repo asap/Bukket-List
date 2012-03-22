f01f17f989eff1796d0817bd9b8ab1d34caa4c6c
William Salazar <was@williamsalazar.com>
2012-03-21 20:52:56 -0400

    Added a required field to the array. So that user knows they have to input something. Thought it is not working right.

--- app/Model/BukketList.php
+++ app/Model/BukketList.php
@@ -2,19 +2,22 @@
 class BukketList extends AppModel{
 	public $name = 'BukketList';	
 	public $validate = array(
-      'title' => array(
-      		'this_field_can_not_be_empty' => array(
-	        	'rule' => 'notEmpty',
-	        	'message' => 'Hey buddy, you forgot to fill this in.'
-          		),
+      'title' => array(     
+            'this_field_can_not_be_empty' => array(
+                'rule' => 'notEmpty',
+                'required' => true,
+                'message' => 'Wait, fill this out first.'
+              ),
+
           	'must_be_unique' => array(
-          		'rule' => 'isUnique',
+          		'rule' => 'isUnique',              
           		'message' => 'You already have this in your bukket list.',
           		)
       ),
       'items' => array(
       		'this_field_can_not_be_empty' => array(
       			'rule' => 'notEmpty',
+            'required' => true,
       			'message' => 'Um... you forgot to fill this one too.'
             )
         )
